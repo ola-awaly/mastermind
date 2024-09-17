f41ac794a28914f6bcafb6686c8894f7dd356461
ola awaly <awaly.ola@gmail.com>
2024-09-17 09:40:00 +0200

    clé pub/private

--- mastermindserver/src/keys/index.js
+++ mastermindserver/src/keys/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 module.exports = {
 	key: (process.env.MODE_ENV = 'prod'
-		? process.env.jwtprivatekey
+		? process.env.JWTPRIVATEKEY
 		: fs.readFileSync(path.join(__dirname, 'private.pem'))),
 	pubkey: (process.env.MODE_ENV = 'prod'
-		? process.env.jwtpublickey
+		? process.env.JWTPUBLICKEY
 		: fs.readFileSync(path.join(__dirname, 'public.pem'))),
 };
 process.env.DB_PROD;
