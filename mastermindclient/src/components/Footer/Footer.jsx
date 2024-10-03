75ddfcc5762781317bdc55a21e51a74c1e26738f
ola awaly <awaly.ola@gmail.com>
2024-10-03 10:48:01 +0200

    edit for testing the reverse proxy

--- mastermindclient/src/components/Footer/Footer.jsx
+++ mastermindclient/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className="bg-clouds-800 text-center font-thin text-xs p-2 w-full text-slate-100">
-			This is the end
+			This is the end awaly
 		</footer>
 	);
 };
